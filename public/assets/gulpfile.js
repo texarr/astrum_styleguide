5aae653a3ec399a42e5e1f17e7f7c2aca7a03dc0
Daniel Pietrucha <d.pietrucha@gmi.pl>
2018-05-16 15:52:32 +0200

    Add default button

---
+++ public/assets/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    // .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('minify', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('scss'));
+});
+
+
+gulp.task('build', ['sass', 'autoprefixer', 'minify']);
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
